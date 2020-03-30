5baa1b756cb1438318837ad14b2f7fefa8e3a4f7
Charan Kottapalli <kottapallicharan@gmail.com>
2020-03-30 16:15:14 -0500

    My First Unity Program -Car and Plane Projects Implemented

---
+++ Project-1/Assets/scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float speed = 20.0f;
+    private float turnspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput* turnspeed);
+    }
+}
